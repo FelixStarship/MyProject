66125ae044cc09be29bd5f42bcd331ae274f2cc3
xuze hu <2633185948@qq.com>
2017-03-24 18:05:10 +0800

    autofac

--- TestMigration/TestMigration/AutofacExt.cs
+++ TestMigration/TestMigration/AutofacExt.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Autofac;
+using TestMigration.Repository;
+using TestMigration.Domain.Interface;
 
 namespace TestMigration
 {
-    public class AutofacExt
+    public static class AutofacExt
     {
-
+        private static IContainer _container;
+        public static void InitAutofac()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterGeneric(typeof(BaseRepository<>)).As(typeof(IRepository<>));
+        }
     }
 }

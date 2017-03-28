d8e04eed1d75f7bc8d77425ae99c47a45a18abce
Administrator <Administrator@MS-201609071955>
2017-03-28 23:47:37 +0800

    Ztree增加

--- TestMigration/TestMigration.Repository/Migrations/201703281223541_Init.Designer.cs
+++ TestMigration/TestMigration.Repository/Migrations/201703281223541_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace TestMigration.Repository.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201703280807441_initial"; }
+            get { return "201703281223541_Init"; }
         }
         
         string IMigrationMetadata.Source
--- TestMigration/TestMigration/Controllers/HomeController.cs
+++ TestMigration/TestMigration/Controllers/HomeController.cs
@@ -16,8 +16,36 @@ namespace TestMigration.Controllers
         {
             this._userRepository = userRepository;
         }
+        private IEnumerable<Category> ztreeObject = new List<Category>
+        {
+           new Category { Id=1, Description="订单查询", Name="订单查询", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=0},
+           new Category { Id=2,Description="待付款订单",Name="待付款订单",IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=1},
+           new Category { Id=3,Description="待核销订单",Name="待核销订单",IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=1},
+           new Category { Id=4,Description="团购订单",Name="团购订单",IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=1},
+           new Category { Id=5,Description="特卖订单",Name="特卖订单",IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=1},
+           new Category { Id=6,Description="秒杀订单",Name="秒杀订单",IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=1},
+           new Category { Id=7, Description="个人中心", Name="个人中心", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=0},
+           new Category { Id=8, Description="修改密码", Name="修改密码", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=7},
+           new Category { Id=9, Description="修改手机号", Name="修改手机号", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=7},
+           new Category { Id=10, Description="修改用户名", Name="修改用户名", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=7},
+           new Category { Id=11, Description="修改昵称", Name="修改昵称", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=7},
+           new Category { Id=12, Description="修改密码", Name="修改密码", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=7},
+           new Category { Id=13, Description="修改密码", Name="修改密码", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=7},
+           new Category { Id=14, Description="修改密码", Name="修改密码", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=7},
+           new Category { Id=15, Description="修改密码", Name="修改密码", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=7},
+           new Category { Id=16, Description="修改密码", Name="修改密码", IsDisplay=true, IsEnabled=true, OrderSort=0, ParentId=7},
+        };
 
+        [HttpPost]
+        public JsonResult GetCategories()
+        {
+            return Json(ztreeObject);
+        }
 
+        public JsonResult addtree(int pid,string name)
+        {
+           return Json(new {Id=19,Name="新添加节点", ParentId=1 });
+        }
         public ActionResult Index()
         {
             //this._userRepository.LoadUsers(1,2);
@@ -44,7 +72,10 @@ namespace TestMigration.Controllers
     /// 类别
     /// </summary>
     public class Category
-    {
+    {   
+
+
+        public int Id { get; set; }
         /// <summary>
         /// 品类名称
         /// </summary>
